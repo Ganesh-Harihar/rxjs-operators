22579c9c6201677d28000f223c17d75f901d7628
ganesh-angularminds <ganesh.harihar@amdev.in>
2021-02-25 17:56:23 +0530

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ObservablesModule } from './pages/observables/observables.module';
 import { PromiseComponent } from './pages/promise/promise.component';
 import { AsyncAwaitComponent } from './pages/async-await/async-await.component';
 import { NotFoundComponent } from './not-found.component';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,17 @@ import { NotFoundComponent } from './not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ObservablesModule
+    ObservablesModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDHBhYaeGPQuVYpCodlXlhr3kkjjCsyHms",
+      authDomain: "saragama-31d9e.firebaseapp.com",
+      databaseURL: "https://saragama-31d9e.firebaseio.com",
+      projectId: "saragama-31d9e",
+      storageBucket: "saragama-31d9e.appspot.com",
+      messagingSenderId: "124296449440",
+      appId: "1:124296449440:web:e9648b3a0c14083ab5e8c5",
+      measurementId: "G-DCZ0ZRQNJY"
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
