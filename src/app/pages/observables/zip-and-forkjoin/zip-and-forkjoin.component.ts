7b978082379ce3424622f34dfae6c2222d055428
ganesh-angularminds <ganesh.harihar@amdev.in>
2021-03-02 18:35:29 +0530

    added multiple api calls handling using promise.all,forkjoin and zip

--- src/app/pages/observables/zip-and-forkjoin/zip-and-forkjoin.component.ts
+++ src/app/pages/observables/zip-and-forkjoin/zip-and-forkjoin.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { forkJoin, fromEvent, zip } from 'rxjs';
-import { pluck, take, withLatestFrom } from 'rxjs/operators';
-
+import { forkJoin, fromEvent, of, throwError, zip } from 'rxjs';
+import { catchError, pluck, take } from 'rxjs/operators';
 @Component({
   selector: 'app-zip-and-forkjoin',
   templateUrl: './zip-and-forkjoin.component.html',
@@ -20,30 +19,57 @@ export class ZipAndForkjoinComponent implements OnInit, AfterViewInit {
   ngOnInit(): void { }
 
   ngAfterViewInit(): void {
-    const nameObservable = fromEvent(this.name.nativeElement, 'change').pipe(pluck('target', 'value'),take(4));
-    const colorObservable = fromEvent(this.color.nativeElement, 'change').pipe(pluck('target', 'value'),take(3));
+    const nameObservable = fromEvent(this.name.nativeElement, 'change').pipe(pluck('target', 'value'), take(4));
+    const colorObservable = fromEvent(this.color.nativeElement, 'change').pipe(pluck('target', 'value'), take(3));
 
     /**
      * Ex - 01 Zip
      */
     zip(nameObservable, colorObservable).subscribe(([name, color]: any) => {
       this.createDiv(name, color, 'zip');
-    })
+    });
 
     /**
      * Ex - 02 ForkJoin
      */
-    forkJoin(nameObservable, colorObservable).subscribe(([name, color]: any) => {
+    forkJoin([nameObservable, colorObservable]).subscribe(([name, color]: any) => {
       this.createDiv(name, color, 'forkjoin');
-    })
+    });
+
+
+    /**
+     * Multiple api request call
+     */
+    zip(
+      fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()),
+      fetch('https://jsonplaceholder.typicode.com/comments').then(response => response.json()),
+      throwError('Error occurred').pipe(catchError(v => of(v))))
+      .subscribe((res) => {
+        console.log('Using zip :', res);
+      });
+
+    forkJoin([
+      fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()),
+      fetch('https://jsonplaceholder.typicode.com/comments').then(response => response.json()),
+      throwError('Error occurred').pipe(catchError(v => of(v)))])
+      .subscribe((res) => {
+        console.log('Using forkJoin :', res);
+      });
+
+    Promise.all([
+      fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()),
+      fetch('https://jsonplaceholder.typicode.com/comments').then(response => response.json()),
+      throwError('Error occurred').toPromise()
+    ]).then(res => console.log('Using promise.all :', res))
+      .catch(_ => console.error('Error occurred in promise.all'));
+
   }
 
   createDiv(data: string, color: string, container: string): void {
-    let el = document.createElement('div');
+    const el = document.createElement('div');
     el.innerText = data;
     el.classList.add(color, 'p-3', 'mx-2');
     document.getElementById(container)?.appendChild(el);
   }
-
 
 }
